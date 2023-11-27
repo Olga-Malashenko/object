1040bf324434acc99ecffee4f38f272e4a8f7057
Olga Malashenko <olga_birjukova@mail.ru>
2023-11-28 01:40:48 +0300

    Task completed

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,21 +1,32 @@
-import propSort from '../app';
+import showAttack from '../app';
 
-test('Should be right order', () => {
-  const obj = {
-    name: 'мечник',
-    health: 10,
-    level: 2,
-    attack: 80,
-    defence: 40,
+test('Should be array', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        // <- обратите внимание, описание "засекречено"
+      },
+    ],
   };
-  const sortOrder = ['name', 'level'];
-  const result = propSort(obj, sortOrder);
+  const result = showAttack(character);
   const expected = [
-    { key: 'name', value: 'мечник' },
-    { key: 'level', value: 2 },
-    { key: 'attack', value: 80 },
-    { key: 'defence', value: 40 },
-    { key: 'health', value: 10 },
+    [8, 'Двойной выстрел', 'Двойной выстрел наносит двойной урон', 'http://...'],
+    [9, 'Нокаутирующий удар', 'Описание недоступно', 'http://...'],
   ];
   expect(result).toEqual(expected);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,12 @@
-// TODO: write your code here
-/*import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2])); */
-
-export default function propSort(obj, sortOrder) {
-  // some logics
-  let propSorted = [];
-  let arr = [];
-  
-  for (const property in obj) {
-    arr.push(property);
-    for (let i = 0; i < sortOrder.length; i++) {
-      if (property === sortOrder[i]) {
-        propSorted[i] = {
-          key: property,
-          value: obj[property],
-        };
-        arr.splice(arr.indexOf(property), 1);
-      }
-    }
+export default function showAttack(obj) {
+  const arr = [];
+  let i = 0;
+  for (const element of obj.special) {
+    const {
+      id, name, description = 'Описание недоступно', icon,
+    } = element;
+    arr[i] = [id, name, description, icon];
+    i++;
   }
-  // сортировка подмассива методом sort
-  const newArr = arr.sort();
-  // помещение в массив, начиная с sortOrder.lenght
- 
-  for (let i = sortOrder.length; i < (sortOrder.length + newArr.length); i++) {
-    propSorted[i] = {
-      key: newArr[i - sortOrder.length],
-      value: obj[newArr[i - sortOrder.length]],
-    };
-  }
-  return propSorted;
+  return arr;
 }
-
