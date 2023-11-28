24fc7f186ab0acdc4c1b9cad2045556725f88aeb
Olga Malashenko <olga_birjukova@mail.ru>
2023-11-28 16:56:07 +0300

    Tests added for 100% coverage

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,6 +1,6 @@
 import sortProperty from '../app';
 
-test('Should be right order', () => {
+test('Should be givin order', () => {
   const obj = {
     name: 'мечник',
     health: 10,
@@ -16,6 +16,26 @@ test('Should be right order', () => {
     { key: 'attack', value: 80 },
     { key: 'defence', value: 40 },
     { key: 'health', value: 10 },
+  ];
+  expect(result).toEqual(expected);
+});
+
+test('Should be alphabet order', () => {
+  const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+  const sortOrder = ['nickName', 'speciality'];
+  const result = sortProperty(obj, sortOrder);
+  const expected = [
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
   ];
   expect(result).toEqual(expected);
 });
